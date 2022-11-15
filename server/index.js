82c87eeb251930495f988badee590c4e9639b648
Kolenchits Sergy <Kolenchitssi@gmail.com>
2022-11-15 19:50:29 +0300

    feat: add folders

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config(); //for access .env
+const express = require("express");
+const sequelize = require("./db");
+
+const PORT = process.env.PORT || 5000; // get port from .env
+const app = express();
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
